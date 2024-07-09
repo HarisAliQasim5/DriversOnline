ec56bf3f3d3d7c56a40a60c425e928b0cea63aa2
HarisQasim5 <harisaliqasim5@gmail.com>
2024-07-09 20:35:59 +0500

    PersonalDriver Page added

--- pages/personalDriver.js
+++ pages/personalDriver.js
@@ -1,9 +1,11 @@
 import PersonalDrivers from '@/components/PersonalDrivers'
+import Navbar from '@/components/layoutScreen/Navbar'
 import React from 'react'
 
 const personalDriver = () => {
   return (
     <div>
+        <Navbar/>
       <PersonalDrivers/>
     </div>
   )
